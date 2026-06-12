657d71bddb25ae0ed85b36658a26db5fc509b323
Sabrina Lee <slee2893@uwo.ca>
2026-06-12 10:00:56 -0400

    Update OpenJ9 branch name in get_source.sh

--- get_source.sh
+++ get_source.sh
@@ -71,7 +71,7 @@ add_source() {
 #
 configure_defaults() {
 	local current_branch="$(git rev-parse --abbrev-ref HEAD)"
-	local openj9_branch=compareunsigned-setatomovx
+	local openj9_branch=compareunsigned
 	local omr_branch=openj9
 
 	# If this repository is on a release branch, use the same branch names
