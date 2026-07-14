e510e8600263212a1224093ff7bfc5f2034ffe9e
Sabrina Lee <slee2893@uwo.ca>
2026-07-14 15:04:01 -0400

    compareunsignedpower-spillfix

--- get_source.sh
+++ get_source.sh
@@ -71,7 +71,7 @@ add_source() {
 #
 configure_defaults() {
 	local current_branch="$(git rev-parse --abbrev-ref HEAD)"
-	local openj9_branch=compareunsigned-power
+	local openj9_branch=compareunsignedpower-spillfix
 	local omr_branch=openj9
 
 	# If this repository is on a release branch, use the same branch names
